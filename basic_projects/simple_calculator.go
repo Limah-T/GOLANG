d646e6ce4d5fd497e8aa66249c4f504f227a6ba3
Limah-T <limahtechnology@yahoo.com>
2025-05-18 17:51:47 +0100

    Worked on two simple projects to practice the basics of GOLANG, which are Temperature Converter and Simple Calculator.

---
+++ basic_projects/simple_calculator.go
@@ -0,0 +1,38 @@
+package main  // This indicates that the file belongs to the main package
+// import ("fmt")  // This is also known as "format"
+
+// func calculator(num1 int, num2 int, op string){
+// 	if op == "+" {
+// 		var result = num1 + num2
+// 		fmt.Printf("Result is %v", 
+// 		result) 
+// 	} else if op == "-"{
+// 		var result = num1 - num2
+// 		fmt.Printf("Result is %v", result)
+// 	} else if op == "*"{
+// 		var result = num1 * num2
+// 		fmt.Printf("Result is %v", result)
+// 	} else if op == "/"{
+// 		if num2 == 0 {
+// 			fmt.Printf("%v cannot be divided by %v", num2, num1)
+// 		} else {
+// 			var result = num1 / num2
+// 			fmt.Printf("Result is %v", result)
+// 		}
+// 	} else {
+// 		fmt.Print("Invalid operator input!")
+// 	}
+// }
+// func main() {
+// 	var(num1 int 
+// 		num2 int
+// 		op string)
+// 	fmt.Print("Enter first num: ")
+// 	fmt.Scan(&num1)
+// 	fmt.Print("Enter the operator to use - +, -, /, *: ")
+// 	fmt.Scan(&op)
+// 	fmt.Print("Enter second num: ")
+// 	fmt.Scan(&num2)
+	
+// 	calculator(num1, num2, op)
+// }
---
+++ basic_projects/temperature_converter.go
@@ -0,0 +1,25 @@
+package main
+import ("fmt")
+const NUMERATOR int = 5
+const DENOMINATOR int = 9
+
+func temperatureConverter(converter string, temperature int){
+	if converter == "C" {
+		var result = (temperature * DENOMINATOR/NUMERATOR) + 32
+		fmt.Printf("Temperature is %v°F", result)
+	} else if converter == "F" {
+		var result = (temperature - 32) * NUMERATOR/DENOMINATOR
+		fmt.Printf("Temperature is %v°C", result)
+	} else {
+		fmt.Print("Invalid converter input")
+	}
+}
+func main() {
+	var converter string
+	var temperature int
+	fmt.Print("Enter the Converter 'F' for Fahrenheit, 'C' for Celsuis: ")
+	fmt.Scan(&converter)
+	fmt.Print("What is the temperature? ")
+	fmt.Scan(&temperature)
+	temperatureConverter(converter, temperature)
+}
